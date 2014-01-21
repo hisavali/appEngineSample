eb7da07a2895f2d1638298cad1ebfd85bfecb129
Hitesh Savaliya <hitesh.savaliya@gmail.com>
2014-01-21 08:57:11 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,117 @@
+#!/usr/bin/env python
+#
+# Copyright 2007 Google Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+import webapp2
+from google.appengine.api import users
+import cgi
+
+import jinja2
+import os
+
+form="""
+<form>
+<select name="q">
+<option value="1">There is only One</option>
+<option>Two</option>
+<option>Three</option>
+</select>
+</br>
+<input type="submit">
+</form>
+"""
+
+form1="""
+<form>
+<label> 
+One
+<input type="radio" name="q" value="One">
+</label></br>
+<label>
+Two
+<input type="radio" name="q" value="Two">
+</label> </br>
+<label>
+Three
+<input type="radio" name="q" value="Three">
+</label>
+</br>
+<input type="submit">
+"""
+
+
+form2="""
+<form method="post">
+Your birthday?
+</br>
+</br>
+<label>
+Month
+<input type="text" name="month">
+</label>
+
+<label>
+Day
+<input type="text" name="day">
+</label>
+
+<label>
+Year
+<input type="text" name="year">
+</label>
+
+</br>
+
+<input type="submit">
+</form>
+"""
+jinja_environment = jinja2.Environment(loader=jinja2.FileSystemLoader(os.path.dirname(__file__)))
+
+class MainHandler(webapp2.RequestHandler):
+	def get(self):
+		template = jinja_environment.get_template('index.html')
+		self.response.out.write(template.render())
+
+class MainHandler1(webapp2.RequestHandler):
+    def get(self):
+
+#    	user = users.get_current_user()
+
+#    	if user:
+#    		self.response.write('Hello,' + user.nickname())
+#    	else:
+#    		self.redirect(users.create_login_url(self.request.uri))
+		self.response.out.write(form)
+
+
+class MainHandler2(webapp2.RequestHandler):
+	"""docstring for MainHandler2"""
+	def get(self):
+		self.response.out.write(form2)
+
+	def post(self):
+		self.response.out.write("That is fine")
+		
+
+class Guestbook(webapp2.RequestHandler):
+	def post(self):
+		self.response.out.write('<html><body>You wrote:<prep>')
+		self.response.out.write(cgi.escape(self.request.get('content')))
+		self.response.out.write('</prep></body></html>')
+
+app = webapp2.WSGIApplication([
+    ('/', MainHandler2),
+    #('/', MainHandler1),
+    ('/sign',Guestbook)], debug=True)
---
+++ test.py
@@ -0,0 +1,38 @@
+
+months = ['January',
+          'February',
+          'March',
+          'April',
+          'May',
+          'June',
+          'July',
+          'August',
+          'September',
+          'October',
+          'November',
+          'December']
+          
+def valid_month(month):
+  if month:
+    cap_month = month.capitalize()
+    if cap_month in months:
+      return cap_month
+    
+    return None
+
+
+def valid_day(day):
+  try:
+    if day:
+        ret = int(day)
+        if ret in range(1,31):
+          return ret
+  except Exception.ValueError:
+    return None
+
+
+def valid_year(year):
+  if year and year.isdigit():
+    ret = int (year)
+    if ret in range(1900,2014):
+      return ret
