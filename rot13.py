1345ad59726d887c91afb0856d060da963c3321b
Hitesh Savaliya <hitesh.savaliya@gmail.com>
2014-01-23 09:21:16 +0000

    Homework Project set 2
    in progress -- Logic for finding 13th character is to do

--- rot13.py
+++ rot13.py
@@ -1,24 +1,29 @@
 
 import webapp2
+from test import escape_html
 
-rot_form="""<textarea rows="10" cols="30" name="text" value="%(new_text)s") method="post">
+rot_form="""
+<form method="post">
+<textarea rows="10" cols="30" name="text">%(new_text)s
 </textarea>
 </br>
 </br>
 
 <input type="submit">
+
+</form>
 """
 class ROT13Handler(webapp2.RequestHandler):
 	def get(self):
-		self.response.headers['Content-Type']='text/plain'
-		self.response.out.write(self.request)
-		# return self.response.out.write(rot_form)
+		# self.response.headers['Content-Type']='text/plain'
+		# self.response.out.write(self.request)
+		return self.write_textForm("")
 
 	def post(self):
-		#orig = self.request.get ('text')
-		#return self.write_textForm("That is fine")
-		self.response.headers['Content-Type']='text/plain'
-		self.response.out.write(self.request)
+		orig = self.request.get ('text')
+		return self.write_textForm(orig.upper())
+		# self.response.headers['Content-Type']='text/plain'
+		# self.response.out.write(self.request)
 
 	def write_textForm(self,text_value=""):
-		return self.response.out.write(rot_form % {"new_text":text_value})
+		return self.response.out.write(rot_form % {"new_text":escape_html(text_value)})
