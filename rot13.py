76f46ef07206447ca1905b5f462660c34b09fa43
Hitesh Savaliya <hitesh.savaliya@apptivation.co.uk>
2014-01-23 19:46:49 +0000

    Few more chnages to ROT13

--- rot13.py
+++ rot13.py
@@ -21,9 +21,25 @@ class ROT13Handler(webapp2.RequestHandler):
 
 	def post(self):
 		orig = self.request.get ('text')
-		return self.write_textForm(orig.upper())
+		return self.write_textForm(self.rot13(orig))
 		# self.response.headers['Content-Type']='text/plain'
 		# self.response.out.write(self.request)
 
 	def write_textForm(self,text_value=""):
 		return self.response.out.write(rot_form % {"new_text":escape_html(text_value)})
+
+
+def rot13(client_str):
+		if client_str:
+			output=[]
+			for c in client_str:
+				if c.isalpha():
+					if ord(c) in range(ord('a'),ord('m')) or range(ord('A'),ord('M')):
+						c = chr(ord(c) + 13)
+					elif ord(c) in range(ord('n'),ord('z')) or range(ord('N'),ord('Z')):
+						c = chr(ord(c) - 13)
+				output.append(c)
+		return str(output)
+
+# print rot13("zzzz")
+
