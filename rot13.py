cb7405092cbc28d63e7a0c0c56275834615d1464
Hitesh Savaliya <hitesh.savaliya@gmail.com>
2014-01-25 08:23:54 +0000

    Template added for ROT13

--- rot13.py
+++ rot13.py
@@ -1,47 +1,52 @@
-
+import os
 import webapp2
-from test import escape_html
 
-rot_form="""
-<form method="post">
-<textarea rows="10" cols="30" name="text">%(new_text)s
-</textarea>
-</br>
-</br>
+import jinja2
 
-<input type="submit">
+jinja_environment = jinja2.Environment(loader=jinja2.FileSystemLoader(os.path.dirname(__file__)), autoescape=True)
 
-</form>
-"""
-class ROT13Handler(webapp2.RequestHandler):
-	def get(self):
-		# self.response.headers['Content-Type']='text/plain'
-		# self.response.out.write(self.request)
-		return self.write_textForm("")
 
-	def post(self):
-		orig = self.request.get ('text')
-		return self.write_textForm(self.rot13(orig))
-		# self.response.headers['Content-Type']='text/plain'
-		# self.response.out.write(self.request)
+def render_str(template, **params):
+    temp = jinja_environment.get_template(template)
+    return temp.render(params)
 
-	def write_textForm(self,text_value=""):
-		return self.response.out.write(rot_form % {"new_text":escape_html(text_value)})
 
-	def rot13(self,client_str):
-		if client_str:
-			output=[]
-			for c in client_str:
-				print 'Processing character %s' %c
-				if c.isalpha():
-					if ord(c) in range(ord('a'),ord('m')+1):
-						c = chr(ord(c) + 13)
-					elif ord(c) in range(ord('A'),ord('M')+1):
-						c = chr(ord(c) + 13)
-					elif ord(c) in range(ord('n'),ord('z')+1):
-						c = chr(ord(c) - 13)
-					elif ord(c) in range(ord('N'),ord('Z')+1):
-						c = chr(ord(c) - 13)
-				
-				output.append(c)
-		return ''.join(output)
+class BaseHandler(webapp2.RequestHandler):
+    def write(self, *a, **kw):
+        self.response.out.write(*a, **kw)
+
+    def render(self, template, **kw):
+        return self.response.out.write(render_str(template, **kw))
+
+
+class ROT13Handler(BaseHandler):
+    def get(self):
+        # self.response.headers['Content-Type'] = 'text/plain'
+        # self.response.out.write(self.request)
+        return self.render("rot13.html")
+
+    def post(self):
+        orig = self.request.get('text')
+        new_text = self.rot13(orig)
+        return self.render("rot13.html", new_text=new_text)
+
+    # self.response.headers['Content-Type']='text/plain'
+    # self.response.out.write(self.request)
+
+
+    def rot13(self, client_str):
+        if client_str:
+            output = []
+            for c in client_str:
+                print 'Processing character %s' % c
+                if c.isalpha():
+                    if ord(c) in range(ord('a'), ord('m') + 1):
+                        c = chr(ord(c) + 13)
+                    elif ord(c) in range(ord('A'), ord('M') + 1):
+                        c = chr(ord(c) + 13)
+                    elif ord(c) in range(ord('n'), ord('z') + 1):
+                        c = chr(ord(c) - 13)
+                    elif ord(c) in range(ord('N'), ord('Z') + 1):
+                        c = chr(ord(c) - 13)
+                output.append(c)
+        return ''.join(output)
