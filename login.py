41bf2101c5e0bb623ea71e5e71b50921ebff36b4
Hitesh Savaliya <hitesh.savaliya@gmail.com>
2014-01-25 15:04:20 +0000

    Problemset2/Homework2 -- Refactore the code

--- login.py
+++ login.py
@@ -12,41 +12,40 @@ class LoginHandler(BaseHandler):
         verify_pwd = self.request.get("verifypwd")
         email = self.request.get("email")
 
-        redirect=True
-        error_username = ""
-        if not self.validate_username(userName):
-            error_username = "wrong username"
+        params = {"user_name": userName, "email": email}
+
+        redirect = True
+        if not validate_username(userName):
+            params["username_error"] = "wrong username"
             redirect = False
 
-        error_password = "Wrong password"
-        if pwd == verify_pwd:
-            if self.validate_password(pwd) and self.validate_password(verify_pwd):
-                error_password = ""
+        if not (validate_password(pwd) and validate_password(verify_pwd)):
+            params["pwd_error"] = "Please enter valid Password"
+            redirect = False
+        elif pwd != verify_pwd:
+            params["verify_pwd_error"] = "Passwords don't match"
+            redirect = False
 
-        if not error_password == "":
-            redirect  = False
-
-        error_email = ""
-        if email and not self.validate_email(email):
-            error_email = "wrong email"
+        if email and not validate_email(email):
+            params["email_error"] = "wrong email"
             redirect = False
 
         if not redirect:
-            params = {"user_name": userName, "pwd": pwd, "verify_pwd": verify_pwd, "email": email,
-                  "username_error": error_username, "pwd_error": error_password, "verify_pwd_error": error_password, "email_error": error_email}
-
             self.render("login.html", **params)
         else:
             self.redirect("/welcome?username="+userName)
 
-    def validate_username(self, username):
-        USER_RE = re.compile("^[a-zA-Z0-9_-]{3,20}$")
-        return USER_RE.match(username)
 
-    def validate_password(self, pwd):
-        USER_RE = re.compile("^.{3,20}$")
-        return USER_RE.match(pwd)
+def validate_username(username):
+    user_re = re.compile('^[a-zA-Z0-9_-]{3,20}$')
+    return user_re.match(username)
 
-    def validate_email(self, email):
-        USER_RE = re.compile("^[\S]+@[\S]+\.[\S]+$")
-        return USER_RE.match(email)
+
+def validate_password(pwd):
+    user_re = re.compile('^.{3,20}$')
+    return user_re.match(pwd)
+
+
+def validate_email(email):
+    user_re = re.compile('^[\S]+@[\S]+\.[\S]+$')
+    return user_re.match(email)
